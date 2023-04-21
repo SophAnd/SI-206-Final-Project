dc419529fe65aa46c50e0b1c4168698e3702d0b8
sophand <sophand@umich.edu>
2023-04-21 14:58:46 -0400

    created nba_stats file

---
+++ nba_stats.py
@@ -0,0 +1,13 @@
+import unittest
+import sqlite3
+import json
+
+def get_data(html_file):
+    pass
+
+def main():
+    print('hi')
+
+if __name__ == "__main__":
+    main()
+    unittest.main(verbosity = 2)
